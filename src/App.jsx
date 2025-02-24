b8d22346c49c653121b070845ac9a414c7172ca1
Vladimir Gatara <vladimir.gatara@carbiotix.com>
2025-02-24 15:50:15 +0100

    start gui

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <h1 class="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div role="main" className="h-screen w-full flex flex-col items-center justify-start pt-[4rem]">
+      <div className="w-[39.6rem] text-center">
+        <h1>We are launching soon!</h1>
+        <p>Subscribe and get notified</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
